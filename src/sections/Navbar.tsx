989b318b7c00aa1ba39321a17668e13a3036a49e
master-utsav <utsavjaiswal890@gmail.com>
2024-09-09 16:27:00 +0530

    adding_experience

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,38 +1,40 @@
 import NavItems from '@/components/NavItems.tsx';
-import React from 'react'
+import React from 'react';
+import styles from './Navbar.module.scss'; // Import CSS module
 
 const Navbar = () => {
-  const [isOpen , isSetOpen] = React.useState<boolean>(false);
+  const [isOpen, isSetOpen] = React.useState<boolean>(false);
 
   const toggleMenu = () => {
     isSetOpen(prevIsOpen => !prevIsOpen);
-  }
+  };
+
   return (
     <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
       <div className='max-w-7xl mx-auto'>
         <div className='flex justify-between items-center py-5 mx-auto c-space'>
-            <a href="/" className='text-neutral-400 font-bold text-xl hover:text-white transition-colors'>
-                UTSAV
-            </a>
-            <button onClick={toggleMenu} className='text-neutral-400 hover:text-white focus:outline-none sm:hidden flex' 
-                    aria-label='Toggle Menu'>
-                <img src={ isOpen ? "assets/close.svg" : "assets/menu.svg" } alt="toggle" className='w-6 h-6' />
-            </button>
+          <a href="/" className='text-neutral-400 font-bold text-xl hover:text-white transition-colors'>
+            UTSAV
+          </a>
 
-            <nav className={`sm:flex hidden`}>
-              <NavItems/>
-            </nav>
+          <button onClick={toggleMenu} className={`${styles['menu-btn']} sm:hidden flex`} aria-label='Toggle Menu'>
+            <div className={`${styles['menu-btn__burger']} ${isOpen ? styles.open : ''}`}></div>
+          </button>
+
+          <nav className={`sm:flex hidden`}>
+            <NavItems/>
+          </nav>
         </div>
       </div>
 
+      {/* Sidebar Menu */}
       <div className={`nav-sidebar bg-black-200 ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
-        <nav className='p-5'>
+        <nav className='p-5 text-center'>
           <NavItems/>
         </nav>
-
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
